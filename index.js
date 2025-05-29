faf0c50562e12a3f2944dff64bf0b448bfa8aa22
Marcelo <marcelopacobello@gmail.com>
2025-05-29 14:26:38 -0300

    chore: configura CORS e atualiza dependências

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const sequelize = require('./models/database');
 const Estoque = require('./models/Estoque');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+app.use(cors());
 
 const estoqueRoutes = require('./routes/estoque');
 
 app.use(express.json());
+app.use(cors());
 
 // Rotas básicas para teste
 app.get('/', (req, res) => {
